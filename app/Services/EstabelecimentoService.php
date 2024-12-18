153cd91d9aea32305bd1438060f6cafcdede6111
akyama.vieira <akyama.vieira@rr.sebrae.com.br>
2024-12-18 16:21:01 -0400

    feat: tratamento de erros

--- app/Services/EstabelecimentoService.php
+++ app/Services/EstabelecimentoService.php
@@ -4,6 +4,7 @@ namespace App\Services;
 
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Cache;
+use Carbon\Carbon;
 use Exception;
 use Log;
 
@@ -27,7 +28,6 @@ class EstabelecimentoService
                     'maximoRegistros' => '50',
                     'versao' => '2.8',
                 ]);
-
                 // Verifica se a resposta foi bem-sucedida
                 if ($response->successful() && $response->json()['status'] === 'OK') {
                     $estabelecimentos = $response->json();
@@ -35,7 +35,33 @@ class EstabelecimentoService
                     Log::info('Dados armazenados no cache com sucesso.');
                 } else {
                     // Log detalhado em caso de erro HTTP
-                    Log::error('Erro na requisição HTTP: ' . $response->json()['mensagem']);
+                    if ($response->successful() && isset($response['status']) && $response['status'] === 'NOK') {
+                        if (isset($response['mensagem']) && strpos($response['mensagem'], 'Somente após') !== false) {
+                            // Extrai o horário de quando a próxima requisição será permitida
+                            $message = $response['mensagem'];
+                            if (preg_match('/Somente após as (\d{2}:\d{2}:\d{2})/', $message, $matches))  {
+                                $nextRequestTimeStr = $matches[1];
+                                //dd($nextRequestTimeStr); // A hora extraída
+                                $nextRequestTime = Carbon::createFromFormat('H:i:s', $nextRequestTimeStr);
+                                //dd($nextRequestTime);
+                                $now = Carbon::now();
+                                // Diferença em segundos (valor absoluto ou relativo)
+                                $waitTime = intval($now->diffInSeconds($nextRequestTime, false));
+
+                                if ($nextRequestTime > $now) {
+                                    Log::warning("Aguarde $waitTime segundos até as {$nextRequestTime->format('H:i:s')} para tentar novamente.");
+                                    //dd($waitTime);
+                                    // Redirecionar para uma página de erro com o tempo restante
+                                    return redirect()->route('test', [
+                                        'waitTime' => $waitTime,
+                                        'nextRequestTime' => $nextRequestTime->format('H:i:s')
+                                    ]);
+                                }
+                            } else {
+                                Log::error("Não foi possível extrair o horário da mensagem: $message");
+                            }
+                        }
+                    }
                 }
             } catch (Exception $e) {
                 // Lida com erros de rede ou outros problemas

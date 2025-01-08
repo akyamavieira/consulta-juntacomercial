c3a648d2a558eb48ad9b8739a92f8a5fe8e18a98
Rodrigo Lacan <rodrigo.lacan@rr.sebrae.com.br>
2025-01-08 11:51:45 -0400

    feat: criando rota e load de provider no projeto laravel

--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,22 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\EstabelecimentosController;
-
- 
-
+use Laravel\Socialite\Facades\Socialite;
 
 Route::get('/', [EstabelecimentosController::class, 'index'])->name('index');
 Route::get('/wait', function () {
     return view('errors.wait');
 })->name("wait");
+
+Route::get('/callback/keycloak', function () {
+    $user = Socialite::driver('keycloak')->user();
+
+    // Aqui você pode salvar o usuário ou criar a lógica de login
+    dd($user);
+
+    //return redirect()->to('/home');
+});
+
+Route::get('/login', function (){
+    return Socialite::driver('keycloak')->redirect();
+});

12f060beabcc50901237c8b4a91561a6e349115c
Rodrigo Lacan <rodrigo.lacan@rr.sebrae.com.br>
2025-01-08 12:41:10 -0400

    feat: implementando debug no socialite

--- config/services.php
+++ config/services.php
@@ -41,6 +41,7 @@ return [
         'redirect' => env('KEYCLOAK_REDIRECT_URL'),
         'base_url' => env('KEYCLOAK_BASE_URL'),
         'realms' => env('KEYCLOAK_REALM'),
+        'debug' => true,
     ],
 
 

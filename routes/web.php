23e84e15c796aa711cdf8660bc7e3b224d4f883a
Rodrigo Lacan <rodrigo.lacan@rr.sebrae.com.br>
2025-01-09 15:02:25 -0400

    refact: adicionando middleware manualmente nas rotas

--- routes/web.php
+++ routes/web.php
@@ -8,16 +8,16 @@ use Laravel\Socialite\Facades\Socialite;
 use Laravel\Socialite\Two\AbstractProvider;
 
 Route::get('/', [EstabelecimentosController::class, 'index'])
-    ->name('index');
+    ->name('index')->middleware([VerifyKeycloakAuth::class]);
 
 Route::get('/wait', function () {
     return view('errors.wait');
-})->name("wait");
+})->name("wait")->middleware([VerifyKeycloakAuth::class]);
 
-Route::get('/callback/keycloak', [KeycloakCallBack::class, 'keycloakCallBack']);
+Route::get('/callback/keycloak', [KeycloakCallBack::class, 'keycloakCallBack'])->withoutMiddleware([VerifyKeycloakAuth::class]);
 
 Route::get('/login', function () {
     /** @var \Laravel\Socialite\Two\AbstractProvider  */
     $driver = Socialite::driver('keycloak');
     return $driver->stateless()->redirect();
-});
+})->withoutMiddleware([VerifyKeycloakAuth::class]);

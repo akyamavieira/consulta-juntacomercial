276f8b02d1841cb229eb8cdb7dfd6543aa234374
Rodrigo Lacan <rodrigo.lacan@rr.sebrae.com.br>
2025-01-09 09:08:35 -0400

    refact: rota de callback do keycloak não envolve o middleware de verificação do usuário logado

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\auth\KeycloakCallBack;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\EstabelecimentosController;
+use App\Http\Middleware\VerifyKeycloakAuth;
 use Laravel\Socialite\Facades\Socialite;
 use Laravel\Socialite\Two\AbstractProvider;
 
@@ -11,7 +12,7 @@ Route::get('/wait', function () {
     return view('errors.wait');
 })->name("wait");
 
-Route::get('/callback/keycloak', [KeycloakCallBack::class, 'keycloakCallBack']);
+Route::get('/callback/keycloak', [KeycloakCallBack::class, 'keycloakCallBack'])->withoutMiddleware([VerifyKeycloakAuth::class]);
 
 Route::get('/login', function (){
     return Socialite::driver('keycloak')->redirect();

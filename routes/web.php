9830a2d309fcf9e6945d118cfcc9fd1fc8943a8a
Akyama S Vieira <akyama.vieira@rr.sebrae.com.br>
2025-01-14 11:26:33 -0400

    nova implementação keycloak

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,5 @@
 <?php
 
-use App\Http\Controllers\auth\KeycloakCallBack;
-use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\EstabelecimentosController;
 use App\Http\Middleware\VerifyKeycloakAuth;
@@ -14,13 +12,6 @@ Route::get('/', [EstabelecimentosController::class, 'index'])
 Route::get('/wait', function () {
     return view('errors.wait');
 })->name("wait")->middleware([VerifyKeycloakAuth::class]);
-
-// Route::get('/callback/keycloak', [KeycloakCallBack::class, 'keycloakCallBack']);
-
-// Route::get('/login', function (){
-//     return Socialite::driver('keycloak')->redirect();
-// });
-// Route::get('/login', [LoginController::class, 'login'])->name('login');
 
 Route::get('login/keycloak', 'LoginController@redirectToKeycloak')->name('login.keycloak');
 Route::get('login/keycloak/callback', 'LoginController@handleKeycloakCallback');

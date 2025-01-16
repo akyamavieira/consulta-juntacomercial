aa7f614a1897962b51cc6593aad986c54deadc7b
Rodrigo Lacan <rodrigo.lacan@rr.sebrae.com.br>
2025-01-16 14:37:18 -0400

    refact: instalando pdo pgsql dentro do container

--- docker/addictions.sh
+++ docker/addictions.sh
@@ -10,4 +10,7 @@ curl -sS https://getcomposer.org/installer -o composer-setup.php
 php composer-setup.php --install-dir=/usr/local/bin --filename=composer
 rm composer-setup.php
 
+apt install -q -y libpq-dev && \
+docker-php-ext-install pdo_pgsql pgsql
+
 composer install && composer update

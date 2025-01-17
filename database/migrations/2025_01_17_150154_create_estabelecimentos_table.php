67d59db1240100b21a26ac932530408f9ea2628b
Akyama S Vieira <akyama.vieira@rr.sebrae.com.br>
2025-01-17 16:19:46 -0400

    criado migrations para EstabelecimentosTable

---
+++ database/migrations/2025_01_17_150154_create_estabelecimentos_table.php
@@ -0,0 +1,56 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEstabelecimentosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('estabelecimentos', function (Blueprint $table) {
+            $table->id();
+            $table->string('cnpj')->unique();
+            $table->string('nomeEmpresarial');
+            $table->string('nomeFantasia')->nullable();
+            $table->date('dataAberturaEstabelecimento')->nullable();
+            $table->date('dataAberturaEmpresa')->nullable();
+            $table->date('dataInicioAtividade')->nullable();
+            $table->string('nuProcessoOrgaoRegistro')->nullable();
+            $table->string('situacaoCadastralRFB_descricao')->nullable();
+            $table->string('opcaoSimplesNacional')->nullable();
+            $table->string('porte')->nullable();
+            $table->string('nuInscricaoMunicipal')->nullable();
+            $table->decimal('capitalSocial', 15, 2)->nullable();
+            $table->boolean('possuiEstabelecimento')->nullable();
+            $table->string('ultimaViabilidadeVinculada')->nullable();
+            $table->string('ultimaViabilidadeAnaliseEndereco')->nullable();
+            $table->date('dataUltimaAnaliseEndereco')->nullable();
+            $table->string('ultimoColetorEstadualWebVinculado')->nullable();
+            $table->string('endereco_cep')->nullable();
+            $table->string('endereco_logradouro')->nullable();
+            $table->string('endereco_codTipoLogradouro')->nullable();
+            $table->string('endereco_numLogradouro')->nullable();
+            $table->string('endereco_complemento')->nullable();
+            $table->string('endereco_bairro')->nullable();
+            $table->string('endereco_codMunicipio')->nullable();
+            $table->string('endereco_uf')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('estabelecimentos');
+    }
+}

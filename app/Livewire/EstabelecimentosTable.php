de817e449f18f82f7bf2762b7312a3887c6aa294
Akyama S Vieira <akyama.vieira@rr.sebrae.com.br>
2025-01-21 09:53:52 -0400

    adicionando o refreshTable no mount do EstabelecimentosTable

--- app/Livewire/EstabelecimentosTable.php
+++ app/Livewire/EstabelecimentosTable.php
@@ -28,6 +28,7 @@ class EstabelecimentosTable extends Component
         \Log::info('Serviço de Estabelecimento injetado: ' . get_class($this->estabelecimentoService));
         // Chama o método getEstabelecimentos para carregar e persistir os dados
         $this->estabelecimentoService->getEstabelecimentos();
+        $this->refreshTable();
     }
 
     public function mostrarTooltip($identificador, $codEvento)

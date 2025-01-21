f6a73a275be3ac978deb7f89d349b9813d45c47a
Akyama S Vieira <akyama.vieira@rr.sebrae.com.br>
2025-01-21 11:55:03 -0400

    getEstabelecimentos agora no controller do index

--- app/Http/Controllers/EstabelecimentosController.php
+++ app/Http/Controllers/EstabelecimentosController.php
@@ -8,8 +8,18 @@ use Exception;
 
 class EstabelecimentosController extends Controller
 {
+    private $estabelecimentoService;
+
+    public function __construct(EstabelecimentoService $estabelecimentoService)
+    {
+        $this->estabelecimentoService = $estabelecimentoService;
+    }
+
     public function index(Request $request)
     {
+        // Chama o método getEstabelecimentos para carregar e persistir os dados
+        $this->estabelecimentoService->getEstabelecimentos();
+
         return view('pages.index');
     }
 }

a6612d69cc9a70a938ef14362ead46d921e8bbe6
Akyama S Vieira <akyama.vieira@rr.sebrae.com.br>
2025-01-22 09:36:55 -0400

    ajuste de rotas keycloak

--- app/Livewire/Logout.php
+++ app/Livewire/Logout.php
@@ -4,18 +4,25 @@ namespace App\Livewire;
 
 use Livewire\Component;
 use Illuminate\Support\Facades\Session;
+use Illuminate\Http\Request;
 
 class Logout extends Component
 {
     
-    public function logout()
+    public function logout(Request $request)
     {
-        // Remover o usuário da sessão
+        // Remove o usuário da sessão
         Session::forget('user');
         Session::invalidate();
-        return redirect('/');
+    
+        // URL de logout do Keycloak
+        $keycloakLogoutUrl = sprintf(
+            'https://lus-homolog.rr.sebrae.com.br/realms/sebrae-corporate-homolog/protocol/openid-connect/logout?redirect_uri=%s',
+            urlencode(route('index')) // Redireciona para a página inicial após o logout
+        );
+    
+        return redirect($keycloakLogoutUrl);
     }
-
     public function render()
     {
         return view('livewire.logout');

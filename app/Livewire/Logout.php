441e73ae508d4e20873ef86b76c329480e534461
Akyama S Vieira <akyama.vieira@rr.sebrae.com.br>
2025-01-22 09:45:03 -0400

    ajuste redirecionamento logout

--- app/Livewire/Logout.php
+++ app/Livewire/Logout.php
@@ -8,19 +8,22 @@ use Illuminate\Http\Request;
 
 class Logout extends Component
 {
-    
+
     public function logout(Request $request)
     {
         // Remove o usuário da sessão
         Session::forget('user');
         Session::invalidate();
-    
+
+        // URL absoluta para redirecionar após o logout
+        $redirectUri = 'https://consulta-jucerr-homolog.rr.sebrae.com.br';
+
         // URL de logout do Keycloak
         $keycloakLogoutUrl = sprintf(
             'https://lus-homolog.rr.sebrae.com.br/realms/sebrae-corporate-homolog/protocol/openid-connect/logout?redirect_uri=%s',
-            urlencode(route('index')) // Redireciona para a página inicial após o logout
+            urlencode($redirectUri) // Substitua pela URL absoluta
         );
-    
+
         return redirect($keycloakLogoutUrl);
     }
     public function render()

8a4df6c17916a3a22565052a13c7886319cfe29a
Rodrigo Lacan <rodrigo.lacan@rr.sebrae.com.br>
2025-01-29 14:33:51 -0400

    refact: trabalhando lógica para campos que estão nulos no banco, e portanto não foram informados pela api

--- resources/views/livewire/estabelecimento-detalhes.blade.php
+++ resources/views/livewire/estabelecimento-detalhes.blade.php
@@ -5,23 +5,23 @@
                 <h2 class="text-xl font-bold mb-4">Detalhes do Estabelecimento</h2>
 
                 <ul class="space-y-2">
-                    <li><strong>CNPJ:</strong> {{ formatCNPJ($detalhesEstabelecimento->cnpj) }}</li>
-                    <li><strong>Nome Empresarial:</strong> {{ $detalhesEstabelecimento->nomeEmpresarial }}</li>
-                    <li><strong>Nome Fantasia:</strong> {{ $detalhesEstabelecimento->nomeFantasia }}</li>
-                    <li><strong>Data Abertura Estabelecimento:</strong> {{ formatData($detalhesEstabelecimento->dataAberturaEstabelecimento) }}</li>
-                    <li><strong>Data Abertura Empresa:</strong> {{ formatData($detalhesEstabelecimento->dataAberturaEmpresa) }}</li>
-                    <li><strong>Data Início Atividade:</strong> {{ formatData($detalhesEstabelecimento->dataInicioAtividade) }}</li>
-                    <li><strong>Número do Processo Órgão Registro:</strong> {{ $detalhesEstabelecimento->nuProcessoOrgaoRegistro }}</li>
-                    <li><strong>Situação Cadastral RFB (Descrição):</strong> {{ $detalhesEstabelecimento->situacaoCadastralRFB_descricao }}</li>
-                    <li><strong>Opção Simples Nacional:</strong> {{ $detalhesEstabelecimento->opcaoSimplesNacional }}</li>
-                    <li><strong>Porte:</strong> {{ $detalhesEstabelecimento->porte }}</li>
-                    <li><strong>Número Inscrição Municipal:</strong> {{ $detalhesEstabelecimento->nuInscricaoMunicipal }}</li>
-                    <li><strong>Capital Social:</strong> {{ formatDinheiro($detalhesEstabelecimento->capitalSocial) }}</li>
-                    <li><strong>Possui Estabelecimento:</strong> {{ $detalhesEstabelecimento->possuiEstabelecimento }}</li>
-                    <li><strong>Última Viabilidade Vinculada:</strong> {{ $detalhesEstabelecimento->ultimaViabilidadeVinculada }}</li>
-                    <li><strong>Última Viabilidade Análise Endereço:</strong> {{ $detalhesEstabelecimento->ultimaViabilidadeAnaliseEndereco }}</li>
-                    <li><strong>Data Última Análise Endereço:</strong> {{ formatData($detalhesEstabelecimento->dataUltimaAnaliseEndereco) }}</li>
-                    <li><strong>Último Coletor Estadual Web Vinculado:</strong> {{ $detalhesEstabelecimento->ultimoColetorEstadualWebVinculado }}</li>
+                    <li><strong>CNPJ:</strong> {{ formatCNPJ($detalhesEstabelecimento->cnpj) ? :'não informado'}}</li>
+                    <li><strong>Nome Empresarial:</strong> {{ $detalhesEstabelecimento->nomeEmpresarial ? :'não informado' }}</li>
+                    <li><strong>Nome Fantasia:</strong> {{ $detalhesEstabelecimento->nomeFantasia ? :'não informado' }}</li>
+                    <li><strong>Data Abertura Estabelecimento:</strong> {{ formatData($detalhesEstabelecimento->dataAberturaEstabelecimento) ? :'não informado' }}</li>
+                    <li><strong>Data Abertura Empresa:</strong> {{ formatData($detalhesEstabelecimento->dataAberturaEmpresa) ? :'não informado' }}</li>
+                    <li><strong>Data Início Atividade:</strong> {{ formatData($detalhesEstabelecimento->dataInicioAtividade) ? :'não informado' }}</li>
+                    <li><strong>Número do Processo Órgão Registro:</strong> {{ $detalhesEstabelecimento->nuProcessoOrgaoRegistro ? :'não informado' }}</li>
+                    <li><strong>Situação Cadastral RFB (Descrição):</strong> {{ $detalhesEstabelecimento->situacaoCadastralRFB_descricao ? :'não informado' }}</li>
+                    <li><strong>Opção Simples Nacional:</strong> {{ $detalhesEstabelecimento->opcaoSimplesNacional ? :'não informado' }}</li>
+                    <li><strong>Porte:</strong> {{ $detalhesEstabelecimento->porte ? :'não informado' }}</li>
+                    <li><strong>Número Inscrição Municipal:</strong> {{ $detalhesEstabelecimento->nuInscricaoMunicipal ? :'não informado' }}</li>
+                    <li><strong>Capital Social:</strong> {{ formatDinheiro($detalhesEstabelecimento->capitalSocial) ? :'não informado' }}</li>
+                    <li><strong>Possui Estabelecimento:</strong> {{ $detalhesEstabelecimento->possuiEstabelecimento ? :'não informado' }}</li>
+                    <li><strong>Última Viabilidade Vinculada:</strong> {{ $detalhesEstabelecimento->ultimaViabilidadeVinculada ? :'não informado' }}</li>
+                    <li><strong>Última Viabilidade Análise Endereço:</strong> {{ $detalhesEstabelecimento->ultimaViabilidadeAnaliseEndereco ? :'não informado' }}</li>
+                    <li><strong>Data Última Análise Endereço:</strong> {{ formatData($detalhesEstabelecimento->dataUltimaAnaliseEndereco) ? :'não informado' }}</li>
+                    <li><strong>Último Coletor Estadual Web Vinculado:</strong> {{ $detalhesEstabelecimento->ultimoColetorEstadualWebVinculado ? :'não informado' }}</li>
                     <li>
                         <p><strong class="text-sm font-semibold">Endereço Completo:</strong></p>
                         <p>

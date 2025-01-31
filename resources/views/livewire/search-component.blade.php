9de90daa0fdf0ef273c22fe9d51aed8a77714620
Rodrigo Lacan <rodrigo.lacan@rr.sebrae.com.br>
2025-01-31 09:25:14 -0400

    refact: diminuindo debounce para 250ms, em função da melhoria da usabilidade

--- resources/views/livewire/search-component.blade.php
+++ resources/views/livewire/search-component.blade.php
@@ -9,7 +9,7 @@
         <input 
             type="search" 
             id="default-search" 
-            wire:model.live.debounce.500ms="search"  
+            wire:model.live.debounce.250ms="search"  
             class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
             placeholder="Realize sua pesquisa" 
             required 

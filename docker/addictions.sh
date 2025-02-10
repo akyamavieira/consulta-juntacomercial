629af7959393e8030c988eacb5053a73cd2dea4c
Rodrigo Lacan <rodrigo.lacan@rr.sebrae.com.br>
2025-02-10 14:54:23 -0400

    feat: instalando lib gd, parametrizando construção

--- docker/addictions.sh
+++ docker/addictions.sh
@@ -5,12 +5,15 @@ curl -fsSL https://deb.nodesource.com/setup_20.x | bash -
 apt-get install -y nodejs
 npm install
 npm run build
+
 # Baixar e instalar o Composer
 curl -sS https://getcomposer.org/installer -o composer-setup.php
 php composer-setup.php --install-dir=/usr/local/bin --filename=composer
 rm composer-setup.php
 
-apt install -q -y libpq-dev && \
+# Instalar extensões do PostgreSQL
+apt install -q -y libpq-dev
 docker-php-ext-install pdo_pgsql pgsql
 
-composer install && composer update
+# Instalar dependências do Composer
+composer install --no-interaction --optimize-autoloader --no-dev
--- dockerfile
+++ dockerfile
@@ -24,7 +24,11 @@ RUN apt-get update && apt-get install -y \
     zip \
     unzip \
     libzip-dev \
-    php8.3-gd \
+    libpng-dev \  
+    libjpeg-dev \ 
+    libfreetype6-dev \ 
+    && docker-php-ext-configure gd --with-freetype --with-jpeg \ 
+    && docker-php-ext-install -j$(nproc) gd \ 
     && rm -rf /var/lib/apt/lists/*
 
 # Limpar pacotes não necessários

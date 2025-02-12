8fa96b88b2662ec5212ef285c4ffc198dfaf33f5
Rodrigo Lacan <rodrigo.lacan@rr.sebrae.com.br>
2025-02-12 13:16:01 -0400

    feat: guardando script com as dependências para verificar seu uso futuro

--- docker/addictions.sh
+++ docker/addictions.sh
@@ -15,5 +15,30 @@ rm composer-setup.php
 apt install -q -y libpq-dev
 docker-php-ext-install pdo_pgsql pgsql
 
+#Instalar Dependências e extensions php
+apt-get update && apt-get install -y \
+    curl \
+    gnupg2 \
+    lsb-release \
+    software-properties-common \
+    git \
+    unzip \
+    libzip-dev \
+    libpng-dev \
+    libjpeg-dev \
+    libfreetype6-dev \
+    && docker-php-ext-configure gd --with-freetype --with-jpeg \
+    && docker-php-ext-install -j$(nproc) gd zip \
+    && docker-php-ext-install zip \
+    && rm -rf /var/lib/apt/lists/*
+
+#Limpar pacotes
+apt-get clean && rm -rf /var/lib/apt/lists/*
+
+#Dando permissão para a pasta padrão do apache
+chown -R www-data:www-data /var/www/html \
+    && find /var/www/html -type d -exec chmod 755 {} \; \
+    && find /var/www/html -type f -exec chmod 644 {} \;
+
 # Instalar dependências do Composer
 composer install --no-interaction --optimize-autoloader --no-dev

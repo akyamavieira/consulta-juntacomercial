69b01d9c7c929ea8774530af19125d903cfc119d
Rodrigo Lacan <rodrigo.lacan@rr.sebrae.com.br>
2025-02-14 10:01:23 -0400

    refact: criando rotas para as agregações necessárias

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,12 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\EstabelecimentosController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::get('/estabelecimentos/all', [EstabelecimentosController::class, 'getAll']);
+
+Route::get('/estabelecimentos/byYear', [EstabelecimentosController::class, 'aggregateByYear']);
+Route::get('/estabelecimentos/bySemester', [EstabelecimentosController::class, 'aggregateBySemester']);
+Route::get('/estabelecimentos/byMonth', [EstabelecimentosController::class, 'aggregateByMonth']);
+Route::get('/estabelecimentos/byWeek', [EstabelecimentosController::class, 'aggregateByWeek']);
+Route::get('/estabelecimentos/byDay', [EstabelecimentosController::class, 'aggregateByDay']);
